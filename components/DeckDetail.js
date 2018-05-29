591f3f215eac48f69d169dac1ce98c2981a3c373
Jansser <jansser_costa@hotmail.com>
2018-05-29 12:49:49 -0300

    feat: Animating Detail View.

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Button,
+  Animated,
+  Easing
+} from 'react-native';
 import DeckHeader from './DeckHeader';
 import { connect } from 'react-redux';
 import { getDeck } from '../utils/api';
 import Loader from './Loader';
 
 class DeckDetail extends Component {
+  componentDidMount() {
+    Animated.timing(new Animated.Value(0), {
+      toValue: 800,
+      easing: Easing.back(),
+      duration: 5000,
+    }).start();
+  }
+
   static navigationOptions = ({ navigation }) => {
     const { title } = navigation.state.params;
 
@@ -18,11 +33,11 @@ class DeckDetail extends Component {
     const { navigation, deck } = this.props;
 
     return (
-      <View>
+      <Animated.View>
         <DeckHeader key={deck.title} deck={deck} />
         <Button onPress={() => navigation.navigate('QuestionForm', { deck: deck })} title='Add Card' />
         <Button onPress={() => navigation.navigate('Quiz', { title: deck.title }) } title='Start Quiz' />
-      </View>
+      </Animated.View>
     );
   }
 }
